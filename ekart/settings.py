d90efc02793537bc60e0fcebc52f3396d584cf52
Arjun C <arjunchandrakumar2017@gmail.com>
2024-08-18 17:05:42 +0530

    Added cname to allowed host 2

--- ekart/settings.py
+++ ekart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default = False, cast=bool)
 
-ALLOWED_HOSTS = ['django-env.eba-wsmsruxn.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env-1.eba-rj2zwr6w.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
