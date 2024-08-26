ae489cad3267eb65a616667a209a615cc1d741a8
Arjun C <arjunchandrakumar2017@gmail.com>
2024-08-26 22:06:58 +0530

    Changed filter and banner carousal

--- store/views.py
+++ store/views.py
@@ -10,19 +10,9 @@ from store.models import Product, ProductGallery, ReviewRating, Variation
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from django.db.models import Q
 from django.db.models import Count, Avg
-# Create your views here.
-def store(request, category_slug=None):
-    categories = None
-    products = None
+from django.db.models import Min, Max
 
-    # Filter by category if provided
-    if category_slug:
-        categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
-    else:
-        products = Product.objects.filter(is_available=True)
-
-    # Filtering by color, size, and price range
+def get_filtered_products(request, products):
     selected_colors = request.GET.getlist('color') if 'color' in request.GET else []
     selected_sizes = request.GET.getlist('size') if 'size' in request.GET else []
     min_price = request.GET.get('min_price') if 'min_price' in request.GET else None
@@ -45,7 +35,23 @@ def store(request, category_slug=None):
         products = products.order_by('-price')
     elif sort_option == 'popularity':
         products = products.annotate(review_count=Count('reviewrating')).order_by('-review_count')
-    
+
+    return products, selected_colors, selected_sizes, min_price, max_price, sort_option
+
+# Create your views here.
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    # Filter by category if provided
+    if category_slug:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+    else:
+        products = Product.objects.filter(is_available=True)
+
+    products, selected_colors, selected_sizes, min_price, max_price, sort_option = get_filtered_products(request, products)
+
     # Pagination
     paginator = Paginator(products, 3)  # Change the number to adjust items per page
     page = request.GET.get('page')
@@ -55,7 +61,26 @@ def store(request, category_slug=None):
     # Fetch available colors and sizes for the filter
     available_colors = Variation.objects.filter(variation_category='color').values_list('variation_value', flat=True).distinct()
     available_sizes = Variation.objects.filter(variation_category='size').values_list('variation_value', flat=True).distinct()
+    for product in paged_products:
+        product.old_price = product.price * 1.1  # 10% more than the current price
 
+    # Calculate min and max prices for the price range filter
+    min_price = products.aggregate(Min('price'))['price__min']
+    max_price = products.aggregate(Max('price'))['price__max']
+
+    # Handle cases where min_price or max_price is None
+    if min_price is None:
+        min_price = 0
+    if max_price is None:
+        max_price = 1000  # Set a default max price if needed
+
+    # Create price range sections
+    step = (max_price - min_price) / 4
+    price_ranges = [round(min_price + i * step) for i in range(5)]  # 5 values to cover 4 sections
+
+    # Retrieve current filter values from the request
+    selected_min_price = request.GET.get('min_price', min_price)
+    selected_max_price = request.GET.get('max_price', max_price)
     context = {
         'products': paged_products,
         'product_count': product_count,
@@ -63,10 +88,13 @@ def store(request, category_slug=None):
         'available_sizes': available_sizes,
         'selected_colors': selected_colors,
         'selected_sizes': selected_sizes,
-        'selected_min_price': min_price,
-        'selected_max_price': max_price,
-        'sort_option': sort_option,  # Add this to maintain the selected sort option in the template
+        'selected_min_price': selected_min_price,
+        'selected_max_price': selected_max_price,
+        'sort_option': sort_option,
+        'price_ranges': price_ranges,  # Pass the generated price ranges
+        'max_price': max_price,  # Pass the max_price to the template
     }
+    print(context)
     return render(request, 'store/store.html', context)
 
 
@@ -103,17 +131,61 @@ def product_detail(request,category_slug,product_slug):
     return render(request, 'store/product_detail.html',context)
 
 def search(request):
+    products = Product.objects.order_by('-created_date')
+
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
-            products = Product.objects.order_by('-created_date').filter(Q(decription__icontains = keyword) | Q(product_name__icontains = keyword))
-            product_count = products.count()
-            
-        context = {
-            'products':products,
-            'product_count' : product_count,
-        }
-    return render(request,'store/store.html',context)
+            products = products.filter(Q(decription__icontains=keyword) | Q(product_name__icontains=keyword))
+
+    # Apply additional filters (color, size, price, etc.) if needed
+    products, selected_colors, selected_sizes, min_price, max_price, sort_option = get_filtered_products(request, products)
+
+    # Pagination
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+    product_count = products.count()
+
+    # Fetch available colors and sizes based on the filtered products
+    available_colors = Variation.objects.filter(product__in=products, variation_category='color').values_list('variation_value', flat=True).distinct()
+    available_sizes = Variation.objects.filter(product__in=products, variation_category='size').values_list('variation_value', flat=True).distinct()
+    for product in paged_products:
+        product.old_price = product.price * 1.1  # 10% more than the current price
+
+    # Calculate min and max prices for the price range filter
+    min_price = products.aggregate(Min('price'))['price__min']
+    max_price = products.aggregate(Max('price'))['price__max']
+
+    # Handle cases where min_price or max_price is None
+    if min_price is None:
+        min_price = 0
+    if max_price is None:
+        max_price = 1000  # Set a default max price if needed
+
+    # Create price range sections
+    step = (max_price - min_price) / 4
+    price_ranges = [round(min_price + i * step) for i in range(5)]  # 5 values to cover 4 sections
+
+    # Retrieve current filter values from the request
+    selected_min_price = request.GET.get('min_price', min_price)
+    selected_max_price = request.GET.get('max_price', max_price)
+
+    context = {
+        'products': paged_products,
+        'product_count': product_count,
+        'available_colors': available_colors,
+        'available_sizes': available_sizes,
+        'selected_colors': selected_colors,
+        'selected_sizes': selected_sizes,
+        'selected_min_price': selected_min_price,
+        'selected_max_price': selected_max_price,
+        'sort_option': sort_option,
+        'price_ranges': price_ranges,  # Pass the generated price ranges
+        'max_price': max_price,  # Pass the max_price to the template
+    }
+
+    return render(request, 'store/store.html', context)
 
 
 def submit_review(request, product_id):
